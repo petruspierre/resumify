b96dd7d86db18ac689fc1141e77cd04e0a645bd8
Petrus Pierre <petrusfla@gmail.com>
2020-06-08 19:46:48 -0300

    Added paragraph

---
+++ src/components/Paragraph/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+import commonStyles from '../../commonStyles';
+
+const styles = StyleSheet.create({
+  inputTitle: {
+    fontSize: 18,
+    fontFamily: commonStyles.fontFamily.primary.regular,
+    color: commonStyles.colors.black,
+    marginBottom: 8,
+  },
+  textArea: {
+    flex: 1,
+    width: '100%',
+    padding: 11,
+    paddingLeft: 16,
+    borderWidth: 1,
+    borderColor: commonStyles.colors.gray,
+    borderRadius: 8,
+    textAlignVertical: 'top',
+    justifyContent: "flex-start"
+  },
+});
+
+export default styles;
