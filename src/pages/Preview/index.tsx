a985831c14c62c72d11842f276befd7864f285bc
Petrus Pierre <petrusfla@gmail.com>
2020-06-09 12:47:24 -0300

    Preview page

---
+++ src/pages/Preview/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { View, TouchableOpacity, Text, FlatList, ScrollView } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import styles from './styles';
+
+interface Params {
+  body: {
+    type: string,
+    title: string,
+    content: string[]
+  }[]
+}
+
+interface CardProps {
+  data: {
+    type: string,
+    title: string,
+    content: string[]
+  }
+}
+
+const Card: React.FC<CardProps> = ({ data }) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.type}>{data.type === 'topic' ? 'tópico' : 'parágrafo'}</Text>
+      {data.type === 'topic' && (
+        <Text style={styles.title}>{data.title}</Text>
+      )}
+      {data.content.map((value, index) => (
+        <Text style={styles.content} key={index}>{'   ' + value}</Text>
+      ))}
+    </View>
+  );
+}
+
+const Preview: React.FC = () => {
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+
+  const routeParams = route.params as Params;
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backArrow} onPress={handleNavigateBack}>
+        <Feather name="arrow-left" size={24} color="#000"/>
+      </TouchableOpacity>
+
+      <ScrollView style={{ flex: 1, width: '100%', marginTop: 32 }} contentContainerStyle={{ paddingHorizontal: 20, paddingBottom: 16 }}>
+        {routeParams.body.map((value, index) => (
+          <Card key={index} data={value}/>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default Preview;
---
+++ src/pages/Preview/styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+import commonStyles from '../../commonStyles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: commonStyles.metrics.statusBarHeight,
+    alignItems: "center"
+  },
+  backArrow: {
+    position: "absolute",
+    top: 8,
+    left: 20,
+    paddingRight: 5,
+    paddingBottom: 5,
+  },
+  card: {
+    width: '100%',
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 16,
+    elevation: 4,
+    justifyContent: "center"
+  },
+  type: {
+    color: commonStyles.colors.gray,
+    fontSize: 18,
+    fontFamily: commonStyles.fontFamily.primary.regular,
+  },
+  title: {
+    color: commonStyles.colors.black,
+    fontSize: 18,
+    fontFamily: commonStyles.fontFamily.primary.medium
+  }, 
+  content: {
+    color: commonStyles.colors.black,
+    fontSize: 16,
+    fontFamily: commonStyles.fontFamily.primary.regular
+  }
+});
+
+export default styles;
