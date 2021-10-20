c33284d8a539f8815528ae73e72693c52fe6758a
Petrus Bento <petrus@intellimize.com>
2021-10-20 18:22:27 -0300

    Change title from MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Resumify',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         elevatedButtonTheme: ElevatedButtonThemeData(
